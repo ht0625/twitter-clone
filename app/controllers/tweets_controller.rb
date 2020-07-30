3ded918d311c50c079b6cb6d3aeaf1187fb80aed
hiroki tamura <hiirookii0625@gmail.com>
2020-07-30 15:59:16 +0900

    バリデーションの定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,8 +6,12 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
   def create
-    Tweet.create(tweet_params)
-    redirect_to new_tweet_path
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to new_tweet_path,notice: "投稿完了！"
+    else
+      render  :new
+    end
   end
   def show
     @tweet = Tweet.find(params[:id])
